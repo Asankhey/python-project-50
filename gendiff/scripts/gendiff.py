bfbf00c297ec61742e206c4dfb3f6ce6a46bc4bf
asankheya <lilitasa@proton.me>
2025-04-09 19:44:02 +0700

    Add JSON file reading and parsing logic

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,26 @@
 import argparse
+import json
+
+
+def read_json(filepath):
+    with open(filepath) as f:
+        return json.load(f)
+
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
+        description="Compares two configuration files and shows a difference."
     )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument(
-        '-f', '--format',
-        help='set format of output',
-        default='stylish'
-    )
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
     args = parser.parse_args()
 
-    print('gendiff called with:', args.first_file, args.second_file, 'Format:', args.format)
+    file1_data = read_json(args.first_file)
+    file2_data = read_json(args.second_file)
 
-if __name__ == '__main__':
+    print(file1_data)
+    print(file2_data)
+
+
+if __name__ == "__main__":
     main()
