e5f2f80a7858d23fa01fbf71b40f19ff01209bd1
asankheya <lilitasa@proton.me>
2025-04-09 18:39:10 +0700

    Add support for --format option

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,13 +2,18 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference."
+        description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        help='set format of output',
+        default='stylish'
+    )
     args = parser.parse_args()
 
-    print("gendiff called with:", args.first_file, args.second_file)
+    print('gendiff called with:', args.first_file, args.second_file, 'Format:', args.format)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
