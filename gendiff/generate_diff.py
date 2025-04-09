ed4f284b5f09939bdbb7cba3f70274f07b3cf3bd
asankheya <lilitasa@proton.me>
2025-04-09 21:00:04 +0700

    Add support for YAML files in generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,14 +1,22 @@
 import json
+import yaml
+import os
 
 
-def read_json(filepath):
+def read_data(filepath):
+    _, ext = os.path.splitext(filepath)
     with open(filepath) as file:
-        return json.load(file)
+        if ext in ['.yaml', '.yml']:
+            return yaml.safe_load(file)
+        elif ext == '.json':
+            return json.load(file)
+        else:
+            raise ValueError(f'Unsupported file format: {ext}')
 
 
 def generate_diff(file_path1, file_path2):
-    data1 = read_json(file_path1)
-    data2 = read_json(file_path2)
+    data1 = read_data(file_path1)
+    data2 = read_data(file_path2)
     keys = sorted(set(data1.keys()) | set(data2.keys()))
 
     result = []
