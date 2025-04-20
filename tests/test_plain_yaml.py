38c499b1e63cbbf43efd8bd59d1a747a16ce19c2
asankheya <lilitasa@proton.me>
2025-04-20 19:32:30 +0700

    style: fix long line in plain yaml test

--- tests/test_plain_yaml.py
+++ tests/test_plain_yaml.py
@@ -12,7 +12,8 @@ def test_plain_yaml_diff():
         "Property 'common.setting2' was removed\n"
         "Property 'common.setting4' was added with value: 'blah blah'\n"
         "Property 'common.setting5' was added with value: [complex value]\n"
-        "Property 'common.setting6.doge.wow' was updated. From [1, 2, 3] to 'so much'\n"
+        "Property 'common.setting6.doge.wow' was updated. "
+        "From [1, 2, 3] to 'so much'\n"
         "Property 'setting4' was removed\n"
         "Property 'setting5' was removed"
     )
